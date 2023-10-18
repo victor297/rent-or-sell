8b01be090bbc7f6adc7b1439b720191346a1ad87
victor297 <38535954+victor297@users.noreply.github.com>
2023-10-18 07:52:29 +0100

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,7 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,5 +14,8 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-initializeApp(firebaseConfig)
-export const db = getFirestore()
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
